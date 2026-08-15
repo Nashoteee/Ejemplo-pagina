531714f456f16c19a1768d390ca9a59d3e56c511
Nashoteee <jivalenzuelat@gmail.com>
2026-08-15 11:21:42 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 const siteConfig = {
     // Textos principales de la Cabecera y Hero
     hero: {
-        titulo: '"La mejor defensa es la <span>prevención</span>."',
+        titulo: "Asesoría Laboral Preventiva para PYMES",
         subtitulo: "Asesoría laboral práctica para micro, pequeñas y medianas empresas. Ayudamos a tomar decisiones correctas y reducir riesgos de multas o conflictos.",
         botonTexto: "Solicitar Evaluación"
     },
